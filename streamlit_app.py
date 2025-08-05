17940decb20a8d8e8e71e0dfdf91c91eae250228
Sonia Sánchez <ssanchezor@gmail.com>
2025-08-05 17:00:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,8 @@ st.write(
   """Choose ther fruits you want to custom Smoothie!
   """
 )
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 # Get the current credentials
 cnx = st.connection("snowflake")
