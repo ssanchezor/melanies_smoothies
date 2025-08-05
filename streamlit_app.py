3675bbf36e7221fd09b58ee2538fb9b679a53dc8
Sonia Sánchez <ssanchezor@gmail.com>
2025-08-05 17:24:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,9 @@ st.write(
 # Get the current credentials
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 #option = st.selectbox(
 #    "What is your favorite fruit?",
 #    ("Banana", "Strawberries", "Peaches"),
